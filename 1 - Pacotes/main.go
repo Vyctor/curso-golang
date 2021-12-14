22e6d4f943c91c1dd35868df2704da425cb23f8e
Vyctor <dev.vyctor@gmail.com>
2021-12-14 13:09:53 -0300

    Pacotes externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+	erro := checkmail.ValidateFormat("123123")
+	fmt.Println(erro)
 }
