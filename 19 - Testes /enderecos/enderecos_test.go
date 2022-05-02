e51e53aba36ee4d2ece9d08cbe6e7631f2cdf95b
vyctorvguimaraes <vyctor.guimaraes@conexia.com.br>
2022-05-02 12:07:35 -0300

    teste unitario com diversos cenarios

--- 19 - Testes /enderecos/enderecos_test.go
+++ 19 - Testes /enderecos/enderecos_test.go
@@ -2,14 +2,53 @@ package enderecos
 
 import "testing"
 
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
 func TestTipoDeEnte(t *testing.T) {
-	enderecoParaTeste := "Rua Paulista 286"
+	cenariosDeTeste := []cenarioDeTeste{
+		{
+			enderecoInserido: "Rua ABC 123",
+			retornoEsperado:  "Rua",
+		},
+		{
+			enderecoInserido: "Avenida Paulista",
+			retornoEsperado:  "Avenida",
+		},
+		{
+			enderecoInserido: "Rodovia dos imigrantes Paulista",
+			retornoEsperado:  "Rodovia",
+		},
+		{
+			enderecoInserido: "Praça das Rosas",
+			retornoEsperado:  "Tipo inválido",
+		},
+		{
+			enderecoInserido: "Estrada 171",
+			retornoEsperado:  "Estrada",
+		},
+		{
+			enderecoInserido: "RUA DOS BOBOS",
+			retornoEsperado:  "Rua",
+		},
+		{
+			enderecoInserido: "AVENIDA REBOLSAS",
+			retornoEsperado:  "Avenida",
+		},
+		{
+			enderecoInserido: "",
+			retornoEsperado:  "Tipo inválido",
+		},
+	}
 
-	tipoDeEnderecoEsperado := "Rua"
-
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
-
-	if tipoDeEnderecoEsperado != tipoDeEnderecoRecebido {
-		t.Error("Esperado:", tipoDeEnderecoEsperado, "Recebido:", tipoDeEnderecoRecebido)
+	for _, cenario := range cenariosDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			if cenario.retornoEsperado != tipoDeEnderecoRecebido {
+				t.Errorf("O tipo recebido é diferente do esperado! Esperado: %s, recebido: %s", cenario.retornoEsperado, tipoDeEnderecoRecebido)
+			}
+		}
 	}
 }
